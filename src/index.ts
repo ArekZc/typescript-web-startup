d85ccf33343005f239fd00659505cdf93b6eb7f8
Arek Zając <arekzc@gmail.com>
2017-03-09 23:41:51 +0100

    generics

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,9 @@
-interface Clock {
-  currentTime: Date
-  tick(): void
-  setTime(date: Date): Clock
+function identity<T>(arg: T): T {
+  return arg
 }
 
-interface ClockConstructor {
-  new (hour: number, minute: number): Clock
+interface GenericIdentityFn<T> {
+  (arg: T): T
 }
 
-class DigitalClock implements Clock {
-  currentTime: Date
-
-  constructor(hour: number, minute: number) {}
-
-  tick() {
-    console.log('beep beep')
-  }
-
-  setTime(date: Date) {
-    return this
-  }
-}
-
-let ctor: ClockConstructor = DigitalClock
-
-const clock = new ctor(12, 12)
-
-interface StringArray {
-  [index: number]: string
-}
-
-const myArray: StringArray = ['Bob', 'Fred']
-
-
+const myIdentity: GenericIdentityFn<number> = identity
