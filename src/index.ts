c476ef8d6a5834a82145e5841a387e320a01eb31
Arek Zając <arekzc@gmail.com>
2017-03-09 23:36:00 +0100

    interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,35 @@
-const isDone: boolean = false
-const age: number = 10
-const fullName: string = 'Arek Zając'
-const what: undefined = undefined
-const iNull: null = null
-const unusable: void = undefined
-const rawObject: object = {}
-
-const obj: Object = {}
-const count: Number = 12
-
-const numbers: number[] = [1, 12]
-const numbersGeneric: Array<number> = [1, 12]
-const tuple: [string, number] = ['category', 12]
-
-enum Color {Red, Green, Blue}
-enum Shadows {Red = 1, Green = 2, Blue = 4}
-
-let notSure: any = 4
-
-function isActive(active: boolean): boolean {
-  return active
+interface Clock {
+  currentTime: Date
+  tick(): void
+  setTime(date: Date): Clock
 }
 
-let activable: (active: boolean) => boolean = isActive
-
-function throwError(message: string): never {
-  throw new Error(message)
+interface ClockConstructor {
+  new (hour: number, minute: number): Clock
 }
+
+class DigitalClock implements Clock {
+  currentTime: Date
+
+  constructor(hour: number, minute: number) {}
+
+  tick() {
+    console.log('beep beep')
+  }
+
+  setTime(date: Date) {
+    return this
+  }
+}
+
+let ctor: ClockConstructor = DigitalClock
+
+const clock = new ctor(12, 12)
+
+interface StringArray {
+  [index: number]: string
+}
+
+const myArray: StringArray = ['Bob', 'Fred']
+
+
