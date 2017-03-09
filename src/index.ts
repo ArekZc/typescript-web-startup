25973880a7938657a3d3185d7e9f8d36f30f5304
Arek Zając <arekzc@gmail.com>
2017-03-09 23:47:03 +0100

    namespaces

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-function identity<T>(arg: T): T {
-  return arg
+namespace Api {
+  export interface UserResponse {
+    id: string
+    name: string
+  }
+
+  export interface UsersResponse {
+    items: UserResponse[],
+    total: number
+  }
 }
 
-interface GenericIdentityFn<T> {
-  (arg: T): T
+const resp: Api.UserResponse = {
+  id: 'f2313a10-ec54-4fdc-abc9-81fe82488454',
+  name: 'Arek'
 }
-
-const myIdentity: GenericIdentityFn<number> = identity
